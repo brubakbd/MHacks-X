52be802e4c95418cac53f83ce7dcb94eb6f89956
Unknown <suvarna.adit@gmail.com>
2017-09-24 02:49:52 -0400

    commited 0123

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 
 import keras
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.utils import np_utils
 from util import load_data
@@ -10,14 +10,8 @@ from util import load_data
 x_train, y_train = load_data()
 x_train = x_train.astype('float32')
 
-model = Sequential()
-model.add(Dense(260, activation='sigmoid', input_shape=x_train.shape[1:]))
-model.add(Dropout(.5))
-model.add(Dense(2600, activation='sigmoid'))
-model.add(Dropout(.5))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(loss = keras.losses.mean_squared_error, optimizer = keras.optimizers.Adam())
-model.fit(x_train, y_train, batch_size=10, epochs=1000, verbose=1, validation_data=(x_train, y_train))
+model = load_model('savedModel')
+model.fit(x_train, y_train, batch_size=20, epochs=10000, verbose=1, validation_data=(x_train, y_train))
+model.save('savedModel')
 score = model.evaluate(x_train, y_train, verbose = 0)
-print(model.predict(x_train))
+print(model.predict(x_train) - y_train)

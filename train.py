02143778a58e65720667e657cf1e7e2f912743a4
Unknown <suvarna.adit@gmail.com>
2017-09-24 00:38:42 -0400

    updated training model

--- train.py
+++ train.py
@@ -12,11 +12,12 @@ x_train = x_train.astype('float32')
 
 model = Sequential()
 model.add(Dense(260, activation='sigmoid', input_shape=x_train.shape[1:]))
+model.add(Dropout(.5))
 model.add(Dense(2600, activation='sigmoid'))
+model.add(Dropout(.5))
 model.add(Dense(1, activation='sigmoid'))
 
-model.compile(loss = keras.losses.mean_squared_error, optimizer = keras.optimizers.Adadelta())
-
-model.fit(x_train, y_train, batch_size=1, epochs=1000, verbose=1, validation_data=(x_train, y_train))
+model.compile(loss = keras.losses.mean_squared_error, optimizer = keras.optimizers.Adam())
+model.fit(x_train, y_train, batch_size=1, epochs=2000, verbose=1, validation_data=(x_train, y_train))
 score = model.evaluate(x_train, y_train, verbose = 0)
 print(model.predict(x_train))

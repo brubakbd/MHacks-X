08fca18aaab6147655925db2828bf8cd18c74f72
Unknown <suvarna.adit@gmail.com>
2017-09-24 13:36:59 -0400

    ahyw44hg

--- util.py
+++ util.py
@@ -32,13 +32,15 @@ def org_data(data, prefs, rprefs):
     count = 0
     for j in data:
         for i in prefs:
-            m = types.index(i['type'])
-            arr[count][m] = 1
+            if types.index(i['type']) != 'dummy':
+                m = types.index(i['type'])
+                arr[count][m] = 1
         if 'rating' in j:
             arr[count][129] = j['rating'] / 5
         for i in rprefs:
-            m = types.index(i['type'])
-            arr[count][m+130] = 1
+            if types.index(i['type']) != 'dummy':
+                m = types.index(i['type'])
+                arr[count][m+130] = 1
         if 'price_level' in j:
             arr[count][259] = j['price_level'] / 4
         count +=1
